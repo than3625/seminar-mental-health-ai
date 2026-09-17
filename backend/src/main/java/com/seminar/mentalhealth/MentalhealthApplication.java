406eb5084556d46f296994fc17366ef13ba0271a
hhdaooo <vuongthihongdao1332005@gmail.com>
2026-09-17 15:57:10 +0700

    Initial project structure

---
+++ backend/src/main/java/com/seminar/mentalhealth/MentalhealthApplication.java
@@ -0,0 +1,13 @@
+package com.seminar.mentalhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalhealthApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seminar/mentalhealth/MentalhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seminar.mentalhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
